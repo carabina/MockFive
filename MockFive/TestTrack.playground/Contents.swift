b2461c58bc8cd2c6a1b3a68efe1de7a70d15a2b2
Daniel Burbank <dburbank@xogrp.com>
2015-11-21 13:00:45 -0500

    Added playground demonstrating struct and class mocks

--- MockFive/TestTrack.playground/Contents.swift
+++ MockFive/TestTrack.playground/Contents.swift
@@ -1,78 +1,73 @@
 import MockFive
 
 
+// Protocol mocking
+struct MyCustomModel { var id: String = "" }
+
 protocol MockworthyProtocol {
-    func noReturn()
-    func empty() -> Int
-    func oneArg(arg: Int)
-    func oneArgRequiredName(arg arg: Int)
-    func oneArgVariadic(arg: Int...)
-    func twoArgVariadic(arg: Int..., otherArg: String)
-    func takesClosure(closure: () -> ()) -> Int
-    func takesAFew(arg: Int, otherArg: Int, stillOther: Int) -> String
-    func takesAFewFirstRequired(arg arg: Int, otherArg: Int)
+    func empty()
+    func oneArgument(name: String)
+    func returnString(name1: String, name2: String) -> String
+    func returnInteger() -> Int
+    
+    func complex(arg1 arg1: Int, model: MyCustomModel, others: Any...) -> (a: Int, b: Int)
 }
 
-struct MockStructOfMockworthyProtocol: Mock, MockworthyProtocol {
+struct MyAwesomeMock: Mock, MockworthyProtocol {
     let instanceId = lock()
     
-    func noReturn() {
-        mock()
+    func empty() {
+        mock() // Log when a method is called
     }
     
-    func empty() -> Int {
-        return mock(returns: 4)
+    func oneArgument(name: String) {
+        mock(name) // Pass arguments to mock method for logging
     }
     
-    func oneArg(arg: Int) {
-        mock(arg)
+    func returnString(name1: String, name2: String) -> String {
+        return mock(name1, name2, returns: "Bob") // Constant return value
     }
     
-    func oneArgRequiredName(arg arg: Int) {
-        mock(arg)
+    func returnInteger() -> Int {
+        return mock() { Int(rand()) } // Closure stub
     }
     
-    func oneArgVariadic(arg: Int..., otherArg: String) {
-        mock(arg)
-    }
-    
-    func takesClosure(closure: () -> ()) -> Int {
-        return mock(closure, returns: 4)
-    }
-    
-    func takesAFew(arg: Int, otherArg: Int, stillOther: Int) -> String {
-        return mock(arg, otherArg, stillOther, returns: "")
-    }
-    
-    func takesAFewFirstRequired(arg arg: Int, otherArg: Int) {
-        return mock(arg, otherArg)
-    }
-    
-    func twoArgVariadic(arg: Int..., otherArg: String) {
-        return mock(arg, otherArg)
-    }
-    
-    func oneArgVariadic(arg: Int...) {
-        return mock()
-    }
+    func complex(arg1 arg1: Int, model: MyCustomModel, others: Any...) -> (a: Int, b: Int) { return mock(arg1, model.id, others, returns: (7, 9)) }
 }
 
-let myMock = MockStructOfMockworthyProtocol()
-myMock.noReturn()
-myMock.empty()
-myMock.oneArg(7)
-myMock.oneArgRequiredName(arg: 0)
-myMock.oneArgVariadic(8, 97, 10, otherArg: "Marley")
-myMock.takesAFew(1, otherArg: 8, stillOther: 33)
-myMock.takesAFewFirstRequired(arg: 0, otherArg: 23)
-myMock.twoArgVariadic(2, 3, 4, otherArg: "Bob")
+var myMock = MyAwesomeMock()
 
-myMock.invocations[0]
-myMock.invocations[1]
-myMock.invocations[2]
-myMock.invocations[3]
-myMock.invocations[4] // Fucked on purpose
-myMock.invocations[5]
-myMock.invocations[6]
-myMock.invocations[7]
-//myMock.invocations[8]
+// Inject, cause behavior, etc...
+myMock.empty()
+myMock.oneArgument("Norton")
+myMock.complex(arg1: 8, model: MyCustomModel(id: "uuid-goes-here"), others: "Blue", 9, 0.21)
+
+// mock.invocations holds invocation records, with arguments
+myMock.invocations[0] // "empty()"
+myMock.invocations[1] // "oneArgument(Norton)"
+myMock.invocations[2] // "complex(arg1: 9, model: uuid-goes-here, others: ["Blue",9,0.21])"
+
+// Stubs with return values do as they should
+var string = myMock.returnString("Billy", name2: "Leeds") // "Bob"
+var integer = myMock.returnInteger() // Integer is random every invocation
+
+// To re-set a mock record, simply assign the invocations array
+myMock.invocations = []
+
+
+// Class mocking
+class MyModel {
+    var modelId = ""
+    var modelValue = 0
+    func modelDesignator() -> String { return "\(modelId): \(modelValue)" }
+}
+
+extension MyModelMockID: Mock {}
+class MyModelMockID: MyModel {
+    let instanceId = lock()
+    
+    override func modelDesignator() -> String { return mock(returns: "Widget: 1") }
+}
+
+var myClassMock = MyModelMockID()
+myClassMock.modelDesignator()
