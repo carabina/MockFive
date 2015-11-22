63c93e8595b4fcd4f71964a3fdf505f520838331
Daniel Burbank <dburbank@xogrp.com>
2015-11-22 01:01:30 -0500

    Tuning

--- MockFive/MockFive.swift
+++ MockFive/MockFive.swift
@@ -1,50 +1,49 @@
 import Foundation
 
 public protocol Mock {
-    var instanceId: String { get }
+    var mockFiveLock: String { get }
     var invocations: [String] { get set }
     
     func mock(arguments: Any?..., function: String)
-    func mock<T>(arguments: Any?..., returns: T, function: String) -> T
     func mock<T>(arguments: Any?..., function: String, returns: () -> T) -> T
     func mock<T: NilLiteralConvertible>(arguments: Any?..., function: String) -> T
 }
 
 extension Mock {
-    public var invocations: [String] { get { return mockRecords[instanceId] ?? [] } set(new) { mockRecords[instanceId] = new } }
-    public func mock(arguments: Any?..., function: String = __FUNCTION__) { logInvocation(stringify(function, arguments: arguments)) }
-    public func mock<T>(arguments: Any?..., returns: T, function: String = __FUNCTION__) -> T { logInvocation(stringify(function, arguments: arguments)); return returns }
-    public func mock<T: NilLiteralConvertible>(arguments: Any?..., function: String = __FUNCTION__) -> T { logInvocation(stringify(function, arguments: arguments)); return nil }
-    public func mock<T>(arguments: Any?..., function: String = __FUNCTION__, returns: () -> T) -> T { logInvocation(stringify(function, arguments: arguments)); return returns() }
+    static public func reset() { globalObjectIDIndex = 0; mockRecords = [:] }
+    public var invocations: [String] { get { return mockRecords[mockFiveLock] ?? [] } set(new) { mockRecords[mockFiveLock] = new } }
+    public func lock(signature: String = __FILE__ + ":\(__LINE__):\(OSAtomicIncrement32(&globalObjectIDIndex))") -> String { return signature }
+    
+    public func mock(arguments: Any?..., function: String = __FUNCTION__) { logInvocation(stringify(function, arguments: arguments, returnType: .None)) }
+    public func mock<T: NilLiteralConvertible>(arguments: Any?..., function: String = __FUNCTION__) -> T { logInvocation(stringify(function, arguments: arguments, returnType: "\(T.self)")); return nil }
+    public func mock<T>(arguments: Any?..., function: String = __FUNCTION__, returns: () -> T) -> T { logInvocation(stringify(function, arguments: arguments, returnType: "\(T.self)")); return returns() }
     
     private func logInvocation(invocation: String) {
         var invocations = [String]()
         invocations.append(invocation)
-        if let existingInvocations = mockRecords[instanceId] { invocations = existingInvocations + invocations }
-        mockRecords[instanceId] = invocations
+        if let existingInvocations = mockRecords[mockFiveLock] { invocations = existingInvocations + invocations }
+        mockRecords[mockFiveLock] = invocations
     }
 }
-
-public func lock(signature: String = __FILE__ + ":\(__LINE__):\(OSAtomicIncrement32(&globalObjectIDIndex))") -> String { return signature }
 
 private var globalObjectIDIndex: Int32 = 0
 private var mockRecords: [String:[String]] = [:]
 
-func + <KeyType, ValueType> (left: Dictionary<KeyType, ValueType>, right: Dictionary<KeyType, ValueType>) -> Dictionary<KeyType, ValueType> {
-    var result: [KeyType:ValueType] = [:]
+func + <T, U> (left: [T:U], right: [T:U]) -> [T:U] {
+    var result: [T:U] = [:]
     for (k, v) in left  { result.updateValue(v, forKey: k) }
     for (k, v) in right { result.updateValue(v, forKey: k) }
     return result
 }
 
-private func stringify(function: String, arguments: [Any?]) -> String {
+private func stringify(function: String, arguments: [Any?], returnType: String?) -> String {
+    var invocation = ""
     let arguments = arguments.map { $0 ?? "nil" } as [Any]
     if .None == function.rangeOfCharacterFromSet(NSCharacterSet(charactersInString: "()")) {
-        return function + "(\(arguments.first ?? "nil"))"
+        invocation = function + "(\(arguments.first ?? "nil"))"
     } else if let _ = function.rangeOfString("()") {
-        return function
+        invocation = function
     } else {
-        var invocation = ""
         let startIndex = function.rangeOfString("(")!.endIndex
         let endIndex = function.rangeOfString(")")!.startIndex
         invocation += function.substringToIndex(startIndex)
@@ -62,12 +61,8 @@ private func stringify(function: String, arguments: [Any?]) -> String {
         }
         
         invocation += ")"
-        
-        
-        
-        if argumentLabels.count - 1 != arguments.count {
-            invocation += " (Expected \(argumentLabels.count - 1), got \(arguments.count))"
-        }
-        return invocation
+        if argumentLabels.count - 1 != arguments.count { invocation += " (Expected \(argumentLabels.count - 1), got \(arguments.count))" }
     }
+    if let returnType = returnType { invocation += " -> \(returnType)" }
+    return invocation
 }
--- TestTrack.playground/Contents.swift
+++ TestTrack.playground/Contents.swift
@@ -2,75 +2,64 @@ import MockFive
 
 
 // Protocol mocking
-struct MyCustomModel { var id: String = "" }
 
-protocol MockworthyProtocol {
-    func empty()
-    func oneArgument(name: String)
-    func returnString(name1: String, name2: String) -> String
-    func returnInteger() -> Int
-    
-    func complex(arg1 arg1: Int, model: MyCustomModel, others: Any...) -> (a: Int, b: Int)
+protocol StringConcatenator {
+    func concatenateString(first: String, second: String) -> String
 }
 
-struct MyAwesomeMock: Mock, MockworthyProtocol {
-    let instanceId = lock()
-    
-    func empty() {
-        mock() // Log when a method is called
-    }
-    
-    func oneArgument(name: String) {
-        mock(name) // Pass arguments to mock method for logging
-    }
-    
-    func returnString(name1: String, name2: String) -> String {
-        return mock(name1, name2, returns: "Bob") // Constant return value
-    }
-    
-    func returnInteger() -> Int {
-        return mock() { Int(rand()) } // Closure stub
-    }
-    
-    func complex(arg1 arg1: Int, model: MyCustomModel, others: Any...) -> (a: Int, b: Int) {
-        return mock(arg1, model.id, others, returns: (7, 9)) // Use printable values for nice output ('model.id', not 'model')
-    }
+struct MockStringConcatenator: StringConcatenator, Mock {
+    let mockFiveLock = "TestTrack.playground:11" // In non-playground, use 'lock()'
+    func concatenateString(first: String, second: String) -> String { return mock(first, second) { "Stub Value" } }
 }
 
-var myMock = MyAwesomeMock()
-
-// Inject, cause behavior, etc...
-myMock.empty()
-myMock.oneArgument("Norton")
-myMock.complex(arg1: 8, model: MyCustomModel(id: "uuid-goes-here"), others: "Blue", 9, 0.21)
-
-// mock.invocations holds invocation records, with arguments
-myMock.invocations[0] // "empty()"
-myMock.invocations[1] // "oneArgument(Norton)"
-myMock.invocations[2] // "complex(arg1: 9, model: uuid-goes-here, others: ["Blue",9,0.21])"
-
-// Stubs with return values do as they should
-var string = myMock.returnString("Billy", name2: "Leeds") // "Bob"
-var integer = myMock.returnInteger() // Integer is random every invocation
-
-// To re-set a mock record, simply assign the invocations array
-myMock.invocations = []
+var myMock = MockStringConcatenator()
+myMock.concatenateString("first", second: "second") // "Stub Value"
+myMock.invocations                                  // [ "concatenateString(_: first, second: second) -> String" ]
 
 
 // Class mocking
-class MyModel {
-    var modelId = ""
-    var modelValue = 0
-    func modelDesignator() -> String { return "\(modelId): \(modelValue)" }
+
+class StringConcatenatorClass {
+    func concatenateString(first: String, second: String) -> String { return first + second }
 }
 
-extension MyModelMockID: Mock {}
-class MyModelMockID: MyModel {
-    let instanceId = lock()
+class MockStringConcatenatorClass: StringConcatenatorClass, Mock {
+    let mockFiveLock = "TestTrack.playground:27" // In non-playground, use 'lock()'
+    override func concatenateString(first: String, second: String) -> String { return mock(first, second) { "Stub Value" } }
+}
+
+var myMockClass = MockStringConcatenatorClass()
+myMockClass.concatenateString("first", second: "second") // "Stub Value"
+myMockClass.invocations                                  // [ "concatenateString(_: first, second: second) -> String" ]
+
+
+
+
+
+struct CustomModel { var id: Int }
+
+protocol MockworthyProtocol {
+    func method()
+    func complexMethod(arg: Int, model: CustomModel, others: Any?...) -> String
+}
+
+struct MockImplementation: MockworthyProtocol, Mock {
+    let mockFiveLock = ""//lock()
     
-    override func modelDesignator() -> String { return mock(returns: "Widget: 1") }
+    func method() {
+        mock()
+    }
+    
+    func complexMethod(arg: Int, model: CustomModel, others: Any?...) -> String {
+        return mock(arg, model.id, others) { "stub string" }
+    }
 }
 
-var myClassMock = MyModelMockID()
-myClassMock.modelDesignator()
-myClassMock.invocations[0]
+var mock = MockImplementation()
+mock.method()
+mock.method()
+mock.complexMethod(7, model: CustomModel(id: 982), others: 7, nil, 0.23, [0,9])
+
+mock.invocations[0]
+mock.invocations[1]
+mock.invocations[2]
